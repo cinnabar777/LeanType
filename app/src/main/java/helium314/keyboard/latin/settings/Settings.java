7ea75dca048cfecc01c1d9a1cb12ac68cfc7d3d5
LeanBitLab <leanbitlab@users.noreply.github.com>
2026-07-11 14:11:30 +0530

    feat: add PREF_DIRECT_IME_SWITCH_TARGET to Settings.java

--- app/src/main/java/helium314/keyboard/latin/settings/Settings.java
+++ app/src/main/java/helium314/keyboard/latin/settings/Settings.java
@@ -92,6 +92,7 @@ public final class Settings implements SharedPreferences.OnSharedPreferenceChang
     public static final String PREF_BLOCK_POTENTIALLY_OFFENSIVE = "block_potentially_offensive";
     public static final String PREF_SHOW_LANGUAGE_SWITCH_KEY = "show_language_switch_key";
     public static final String PREF_LANGUAGE_SWITCH_KEY = "language_switch_key";
+    public static final String PREF_DIRECT_IME_SWITCH_TARGET = "direct_ime_switch_target";
     public static final String PREF_SHOW_EMOJI_KEY = "show_emoji_key";
     public static final String PREF_VARIABLE_TOOLBAR_DIRECTION = "var_toolbar_direction";
     public static final String PREF_ADDITIONAL_SUBTYPES = "additional_subtypes";
